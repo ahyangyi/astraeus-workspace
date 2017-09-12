8a7c0b71e9e3fc0f230c0d1febd0c076593fee44
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2017-09-13 00:06:40 +0800

    GNU MultiPrecision Library

---
+++ base-toolchain/gmp/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION=6.1.2
+
+[ -e gmp-${VERSION}.tar.xz ] || wget https://gmplib.org/download/gmp/gmp-${VERSION}.tar.xz
+tar xfv gmp-${VERSION}.tar.xz
+cd gmp-${VERSION}
+mkdir -p build
+cd build
+../configure --with-gnu-ld --prefix=${PWD}/../prefix/ --enable-shared=no CFLAGS="${CFLAGS} -fPIC -DPIC" --host=x86_64-unknown-linux
+make ${MAKEOPTS} && make check && make install
+rm ../prefix/lib/libgmp.la
---
+++ env/env.sh
@@ -0,0 +1,1 @@
+CFLAGS='-O2 -Wno-builtin-macro-redefined -D__DATE__="Astraeus" -D__TIMESTAMP__="Astaeus" -D__TIME__="Astraeus"'
